89fb13456083ce73916a84ad17810a7195bf0598
sachinm78 <sachinm78@gmail.com>
2020-12-15 15:14:28 -0600

    fixed newPlatform function; tweeked game to make it more challenging. Game logic is working well; need to add start game button

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let isGameOver = false
+    let speed = 3
     let platformCount = 5
     let platforms = []
+    let score = 0
     let doodlerLeftSpace = 50
     let startPoint = 150
     let doodlerBottomSpace = startPoint
+    const gravity = 0.9
     let upTimerId
     let downTimerId
     let isJumping = true
@@ -14,51 +17,51 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId
     let rightTimerId
-    let score = 0
   
     class Platform {
-        constructor(newPlatBottom) {
-          this.left = Math.random() * 315
-          this.bottom = newPlatBottom
-          this.visual = document.createElement('div')
-    
-          const visual = this.visual
-          visual.classList.add('platform')
-          visual.style.left = this.left + 'px'
-          visual.style.bottom = this.bottom + 'px'
-          grid.appendChild(visual)
-        }
+      constructor(newPlatBottom) {
+        this.left = Math.random() * 315
+        this.bottom = newPlatBottom
+        this.visual = document.createElement('div')
+  
+        const visual = this.visual
+        visual.classList.add('platform')
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
+        grid.appendChild(visual)
+      }
     }
-    
+  
+  
     function createPlatforms() {
-    for(let i =0; i < platformCount; i++) {
+      for(let i =0; i < platformCount; i++) {
         let platGap = 600 / platformCount
         let newPlatBottom = 100 + i * platGap
         let newPlatform = new Platform (newPlatBottom)
         platforms.push(newPlatform)
         console.log(platforms)
-        }
+      }
     }
-
+  
     function movePlatforms() {
-        if (doodlerBottomSpace > 200) {
-            platforms.forEach(platform => {
-                platform.bottom -= 4
-                let visual = platform.visual
-                visual.style.bottom = platform.bottom + 'px'
-
-                if(platform.bottom < 10) {
-                let firstPlatform = platforms[0].visual
-                firstPlatform.classList.remove('platform')
-                platforms.shift()
-                console.log(platforms)
-                score++
-                var newPlatform = new Platform(600)
-                platforms.push(newPlatform)
-                }
-            }) 
-        }
-        
+      if (doodlerBottomSpace > 200) {
+          platforms.forEach(platform => {
+            platform.bottom -= 4
+            let visual = platform.visual
+            visual.style.bottom = platform.bottom + 'px'
+  
+            if(platform.bottom < 10) {
+              let firstPlatform = platforms[0].visual
+              firstPlatform.classList.remove('platform')
+              platforms.shift()
+              console.log(platforms)
+              score++
+              var newPlatform = new Platform(600)
+              platforms.push(newPlatform)
+            }
+        }) 
+      }
+      
     }
   
     function createDoodler() {
@@ -71,36 +74,36 @@ document.addEventListener('DOMContentLoaded', () => {
   
     function fall() {
         isJumping = false
-          clearInterval(upTimerId)
-          downTimerId = setInterval(function () {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function () {
             doodlerBottomSpace -= 5
             doodler.style.bottom = doodlerBottomSpace + 'px'
             if (doodlerBottomSpace <= 0) {
-              gameOver()
+            gameOver()
             }
             platforms.forEach(platform => {
-              if (
+            if (
                 (doodlerBottomSpace >= platform.bottom) &&
                 (doodlerBottomSpace <= (platform.bottom + 15)) &&
                 ((doodlerLeftSpace + 60) >= platform.left) && 
                 (doodlerLeftSpace <= (platform.left + 85)) &&
                 !isJumping
                 ) {
-                  console.log('tick')
-                  startPoint = doodlerBottomSpace
-                  jump()
-                  console.log('start', startPoint)
-                  isJumping = true
+                console.log('tick')
+                startPoint = doodlerBottomSpace
+                jump()
+                console.log('start', startPoint)
+                isJumping = true
                 }
             })
-      
-          },20)
+    
+        },20)
     }
-      
+  
     function jump() {
-        clearInterval(downTimerId)
-        isJumping = true
-        upTimerId = setInterval(function () {
+      clearInterval(downTimerId)
+      isJumping = true
+      upTimerId = setInterval(function () {
         console.log(startPoint)
         console.log('1', doodlerBottomSpace)
         doodlerBottomSpace += 20
@@ -108,49 +111,49 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('2',doodlerBottomSpace)
         console.log('s',startPoint)
         if (doodlerBottomSpace > (startPoint + 200)) {
-            fall()
-            isJumping = false
+          fall()
+          isJumping = false
         }
-        },30)
+      },30)
     }
   
     function moveLeft() {
-        if (isGoingRight) {
-            clearInterval(rightTimerId)
-            isGoingRight = false
-        }
-        isGoingLeft = true
-        leftTimerId = setInterval(function () {
-            if (doodlerLeftSpace >= 0) {
-              console.log('going left')
-              doodlerLeftSpace -=5
-               doodler.style.left = doodlerLeftSpace + 'px'
-            } else moveRight()
-        },20)
+      if (isGoingRight) {
+          clearInterval(rightTimerId)
+          isGoingRight = false
+      }
+      isGoingLeft = true
+      leftTimerId = setInterval(function () {
+          if (doodlerLeftSpace >= 0) {
+            console.log('going left')
+            doodlerLeftSpace -=5
+             doodler.style.left = doodlerLeftSpace + 'px'
+          } else moveRight()
+      },20)
     }
-    
+  
     function moveRight() {
-        if (isGoingLeft) {
-            clearInterval(leftTimerId)
-            isGoingLeft = false
-        }
-        isGoingRight = true
-        rightTimerId = setInterval(function () {
-          //changed to 313 to fit doodle image
-          if (doodlerLeftSpace <= 313) {
-            console.log('going right')
-            doodlerLeftSpace +=5
-            doodler.style.left = doodlerLeftSpace + 'px'
-          } else moveLeft()
-        },20)
+      if (isGoingLeft) {
+          clearInterval(leftTimerId)
+          isGoingLeft = false
+      }
+      isGoingRight = true
+      rightTimerId = setInterval(function () {
+        //changed to 313 to fit doodle image
+        if (doodlerLeftSpace <= 313) {
+          console.log('going right')
+          doodlerLeftSpace +=5
+          doodler.style.left = doodlerLeftSpace + 'px'
+        } else moveLeft()
+      },20)
     }
     
     function moveStraight() {
-    isGoingLeft = false
-    isGoingRight = false
-    clearInterval(leftTimerId)
-    clearInterval(rightTimerId)
-    }    
+      isGoingLeft = false
+      isGoingRight = false
+      clearInterval(leftTimerId)
+      clearInterval(rightTimerId)
+    }
   
     //assign functions to keyCodes
     function control(e) {
@@ -179,13 +182,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
   
     function start() {
-        if (!isGameOver) {
-          createPlatforms()
-          createDoodler()
-          setInterval(movePlatforms,30)
-          jump(startPoint)
-          document.addEventListener('keyup', control)
-        } 
+      if (!isGameOver) {
+        createPlatforms()
+        createDoodler()
+        setInterval(movePlatforms,30)
+        jump(startPoint)
+        document.addEventListener('keyup', control)
+      } 
     }
     start()
 })
