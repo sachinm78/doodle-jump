b8cffa242f06eac695a0c107e3454153ae21ed2c
sachinm78 <sachinm78@gmail.com>
2020-12-15 14:48:01 -0600

    added functions for moveLeft, moveRight and moveStraight; added score variable; need to fix newPlatform function, currently not getting new platforms to appear

--- index.js
+++ index.js
@@ -10,32 +10,56 @@ document.addEventListener('DOMContentLoaded', () => {
     let upTimerId
     let downTimerId
     let isJumping = true
+    let isGoingLeft = false
+    let isGoingRight = false
+    let leftTimerId
+    let rightTimerId
+    let score = 0
   
     class Platform {
-      constructor(newPlatBottom) {
-        this.left = Math.random() * 315
-        this.bottom = newPlatBottom
-        this.visual = document.createElement('div')
-  
-        const visual = this.visual
-        visual.classList.add('platform')
-        visual.style.left = this.left + 'px'
-        visual.style.bottom = this.bottom + 'px'
-        grid.appendChild(visual)
-      }
+        constructor(newPlatBottom) {
+          this.left = Math.random() * 315
+          this.bottom = newPlatBottom
+          this.visual = document.createElement('div')
+    
+          const visual = this.visual
+          visual.classList.add('platform')
+          visual.style.left = this.left + 'px'
+          visual.style.bottom = this.bottom + 'px'
+          grid.appendChild(visual)
+        }
     }
-  
-  
+    
     function createPlatforms() {
-      for(let i =0; i < platformCount; i++) {
+    for(let i =0; i < platformCount; i++) {
         let platGap = 600 / platformCount
         let newPlatBottom = 100 + i * platGap
         let newPlatform = new Platform (newPlatBottom)
         platforms.push(newPlatform)
         console.log(platforms)
-      }
+        }
     }
-  
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+
+                if(platform.bottom < 10) {
+                let firstPlatform = platforms[0].visual
+                firstPlatform.classList.remove('platform')
+                platforms.shift()
+                console.log(platforms)
+                score++
+                var newPlatform = new Platform(600)
+                platforms.push(newPlatform)
+                }
+            }) 
+        }
+        
+    }
   
     function createDoodler() {
       grid.appendChild(doodler)
@@ -90,6 +114,43 @@ document.addEventListener('DOMContentLoaded', () => {
         },30)
     }
   
+    function moveLeft() {
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+        isGoingLeft = true
+        leftTimerId = setInterval(function () {
+            if (doodlerLeftSpace >= 0) {
+              console.log('going left')
+              doodlerLeftSpace -=5
+               doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
+        },20)
+    }
+    
+    function moveRight() {
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+          //changed to 313 to fit doodle image
+          if (doodlerLeftSpace <= 313) {
+            console.log('going right')
+            doodlerLeftSpace +=5
+            doodler.style.left = doodlerLeftSpace + 'px'
+          } else moveLeft()
+        },20)
+    }
+    
+    function moveStraight() {
+    isGoingLeft = false
+    isGoingRight = false
+    clearInterval(leftTimerId)
+    clearInterval(rightTimerId)
+    }    
   
     //assign functions to keyCodes
     function control(e) {
@@ -118,13 +179,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
   
     function start() {
-      if (!isGameOver) {
-        createPlatforms()
-        createDoodler()
-        setInterval(movePlatforms,30)
-        jump(startPoint)
-        document.addEventListener('keyup', control)
-      } 
+        if (!isGameOver) {
+          createPlatforms()
+          createDoodler()
+          setInterval(movePlatforms,30)
+          jump(startPoint)
+          document.addEventListener('keyup', control)
+        } 
     }
     start()
 })
