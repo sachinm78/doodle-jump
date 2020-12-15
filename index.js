d41572eabc4f76580390eb53900f77b1e137740d
sachinm78 <sachinm78@gmail.com>
2020-12-15 13:57:54 -0600

    fixed error that was preventing platforms from displaying; started writing function control logic to move doodler with keyboard arrows

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
-    let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
     let platforms = []
+    let doodlerLeftSpace = 50
+    let startPoint = 150
+    let doodlerBottomSpace = startPoint
     let upTimerId
     let downTimerId
+    let isJumping = true
   
     class Platform {
-        constructor(newPlatBottom) {
-          this.left = Math.random() * 315
-          this.bottom = newPlatBottom
-          this.visual = document.createElement('div')
-    
-          const visual = this.visual
-          visual.classList.add('platform')
-          visual.style.left = this.left + 'px'
-          visual.style.bottom = this.bottom + 'px'
-          grid.appendChild(visual)
-        }
+      constructor(newPlatBottom) {
+        this.left = Math.random() * 315
+        this.bottom = newPlatBottom
+        this.visual = document.createElement('div')
+  
+        const visual = this.visual
+        visual.classList.add('platform')
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
+        grid.appendChild(visual)
+      }
     }
-    
+  
+  
+    function createPlatforms() {
+      for(let i =0; i < platformCount; i++) {
+        let platGap = 600 / platformCount
+        let newPlatBottom = 100 + i * platGap
+        let newPlatform = new Platform (newPlatBottom)
+        platforms.push(newPlatform)
+        console.log(platforms)
+      }
+    }
+  
+  
     function createDoodler() {
       grid.appendChild(doodler)
       doodler.classList.add('doodler')
@@ -29,76 +44,68 @@ document.addEventListener('DOMContentLoaded', () => {
       doodler.style.left = doodlerLeftSpace + 'px'
       doodler.style.bottom = doodlerBottomSpace + 'px'
     }
-
-    function createPlatforms() {
-        for(let i =0; i < platformCount; i++) {
-          let platGap = 600 / platformCount
-          let newPlatBottom = 100 + i * platGap
-          let newPlatform = new Platform (newPlatBottom)
-          platforms.push(newPlatform)
-          console.log(platforms)
+  
+  function fall() {
+    isJumping = false
+      clearInterval(upTimerId)
+      downTimerId = setInterval(function () {
+        doodlerBottomSpace -= 5
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+        if (doodlerBottomSpace <= 0) {
+          gameOver()
         }
-    }
-
+  
+      },20)
+  }
+  
     function jump() {
-        clearInterval(downTimerId)
-        upTimerId = setInterval(function () {
-          doodlerBottomSpace += 20
-          doodler.style.bottom = doodlerBottomSpace + 'px'
-          if (doodlerBottomSpace > (startPoint + 200)) {
-            fall()
-          }
-        },30)
+      clearInterval(downTimerId)
+      isJumping = true
+      upTimerId = setInterval(function () {
+        doodlerBottomSpace += 20
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+        if (doodlerBottomSpace > (startPoint + 200)) {
+          fall()
+          isJumping = false
+        }
+      },30)
     }
-
-    function fall() {
-        clearInterval(upTimerId)  
-        downTimerId = setInterval(function () {
-            doodlerBottomSpace -= 5
-            doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace <= 0) {
-                gameOver()
-            }
-        },20)
+  
+  
+    //assign functions to keyCodes
+    function control(e) {
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+      if(e.key === 'ArrowLeft') {
+        moveLeft()
+      } else if (e.key === 'ArrowRight') {
+        moveRight()
+      } else if (e.key === 'ArrowUp') {
+        moveStraight()
+      }
     }
-
-    function movePlatforms() {
-        if (doodlerBottomSpace > 200) {
-            platforms.forEach(platform => {
-              platform.bottom -= 4
-              let visual = platform.visual
-              visual.style.bottom = platform.bottom + 'px'
-    
-              if(platform.bottom < 10) {
-                let firstPlatform = platforms[0].visual
-                firstPlatform.classList.remove('platform')
-                platforms.shift()
-                console.log(platforms)
-                score++
-                var newPlatform = new Platform(600)
-                platforms.push(newPlatform)
-              }
-        }) 
-    }
-
-
-
-    function start() {
-        if (!isGameOver) {
-          createDoodler()
-          createPlatforms()
-          setInterval(movePlatforms,30)
-          jump()
-          
-        } 
-    }
-
+  
     function gameOver() {
-        isGameOver = true
-        clearInterval(upTimerId)
-        clearInterval(downTimerId)
+      isGameOver = true
+      while (grid.firstChild) {
+        console.log('remove')
+        grid.removeChild(grid.firstChild)
+      }
+      grid.innerHTML = score
+      clearInterval(upTimerId)
+      clearInterval(downTimerId)
+      clearInterval(leftTimerId)
+      clearInterval(rightTimerId)
     }
-
+  
+  
+    function start() {
+      if (!isGameOver) {
+        createPlatforms()
+        createDoodler()
+        setInterval(movePlatforms,30)
+        jump(startPoint)
+        document.addEventListener('keyup', control)
+      } 
+    }
     start()
-    }
 })
